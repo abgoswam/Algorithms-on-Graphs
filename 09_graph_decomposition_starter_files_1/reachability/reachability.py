ed5345ab3d9fa115de9c7ffd064e2fdf85d3eb4c
Abhishek Goswami <abgoswam@gmail.com>
2018-10-07 11:42:06 -0700

    week 1

--- 09_graph_decomposition_starter_files_1/reachability/reachability.py
+++ 09_graph_decomposition_starter_files_1/reachability/reachability.py
@@ -3,9 +3,23 @@
 import sys
 
 
-def reach(adj, x, y):
-    #write your code here
+def dfs(adj, v, t, visited):
+    visited[v] = True
+    neighbors = adj[v]
+    for neigh in neighbors:
+        if neigh == t:
+            return 1
+        if not visited[neigh]:
+            res = dfs(adj, neigh, t, visited)
+            if res:
+                return 1
     return 0
+
+
+def reach(adj, x, y):
+    # write your code here
+    visited = [False for _ in range(len(adj))]
+    return dfs(adj, x, y, visited)
 
 
 if __name__ == '__main__':
@@ -14,6 +28,7 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    # print("edge : {0}".format(edges))
     x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
     x, y = x - 1, y - 1
